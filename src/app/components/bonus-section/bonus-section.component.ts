b4e4b9c4b9a11572a7b394697a8a43b42018cc3c
phhephhe <beridzedavit07@gmail.com>
2024-09-16 22:40:59 +0400

    add prime ng carousel

--- src/app/components/bonus-section/bonus-section.component.ts
+++ src/app/components/bonus-section/bonus-section.component.ts
@@ -14,23 +14,20 @@ export class BonusSectionComponent implements OnInit{
     this.updateRandomCheckpoint()
   }
   checkpoints = [
-    { position: 0, amount: 1000 },
-    { position: 25, amount: 5000 },
-    { position: 50, amount: 10000 },
-    { position: 75, amount: 20000 },
-    { position: 100, amount: 50000 }
+    { position: 0, amount: 50 },
+    { position: 25, amount: 150 },
+    { position: 50, amount: 300 },
+    { position: 75, amount: 500 },
+    { position: 100, amount: 1000 }
   ];
 
-  // Variable to track the current checkpoint (1-based index)
   currentCheckpoint = 0;
 
-  // Calculate the progress width based on the current checkpoint
   get progressWidth() {
     console.log(this.currentCheckpoint * (100 / (this.checkpoints.length - 1)),"+")
     return this.currentCheckpoint * (100 / (this.checkpoints.length - 1));
   }
 
-  // Function to randomly select a checkpoint
   updateRandomCheckpoint() {
     this.currentCheckpoint = Math.floor(Math.random() * this.checkpoints.length);
     console.log(this.currentCheckpoint,'-=')
--- src/app/components/slider-card/slider-card.component.ts
+++ src/app/components/slider-card/slider-card.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-
+import { CarouselModule } from 'primeng/carousel';
 @Component({
   selector: 'app-slider-card',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './slider-card.component.html',
   styleUrl: './slider-card.component.scss'
 })
 export class SliderCardComponent {
 
+  responsiveOptions = [
+    {
+      breakpoint: '768px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
+
+
+  slides = [{
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    },
+    {
+      id: '1001',
+      code: 'nvklal433',
+      name: 'Black Watch',
+      description: 'Product Description',
+      image: 'black-watch.jpg',
+      price: 72,
+      category: 'Accessories',
+      quantity: 61,
+      inventoryStatus: 'OUTOFSTOCK',
+      rating: 4
+    },
+  ]
 }
