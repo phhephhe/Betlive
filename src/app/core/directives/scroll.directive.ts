c367bb784663c8cffeea8b26ee34275b356292b1
phhephhe <beridzedavit07@gmail.com>
2024-09-17 14:04:06 +0400

    add scroling directive

---
+++ src/app/core/directives/scroll.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appScroll]',
+  standalone: true
+})
+export class ScrollDirective {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY || 0; 
+    console.log(scrollPosition,'scrollposition')
+    
+    if (scrollPosition > 0) {
+      this.renderer.addClass(this.el.nativeElement, 'scrolled');
+    } else {
+      this.renderer.removeClass(this.el.nativeElement, 'scrolled');
+    }
+  }
+}
